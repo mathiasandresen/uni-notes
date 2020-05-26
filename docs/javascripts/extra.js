7063679b6992bb19cf5e81c7196459b83b225422
Mathias Andresen <dev@andresen.xyz>
2020-05-26 15:51:09 +0200

    add color and xcolor to js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,5 +1,7 @@
 window.MathJax = {
     TeX: {
-        extensions: ["AMSmath.js", "AMSsymbols.js", "mediawiki-texvc.js"]
+        extensions: ["AMSmath.js", "AMSsymbols.js", "mediawiki-texvc.js"],
+        loader: {load: ['[tex]/color', '[tex]/xcolor']},
+        packages: {'[+]': ['color', 'xcolor']}
     }
 };
